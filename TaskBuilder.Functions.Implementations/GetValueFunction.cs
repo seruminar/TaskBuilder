e6b757c4a947b7f7e8b50b981f5159faec273ea3
Yuriy Sountsov <YuriyS@kentico.com>
2019-01-29 01:51:28 -0500

    Moved function implementations to separate assembly

---
+++ TaskBuilder.Functions.Implementations/GetValueFunction.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using CMS.DataEngine;
+
+using TaskBuilder.Attributes;
+using TaskBuilder.ValueBuilders;
+
+namespace TaskBuilder.Functions.Implementations
+{
+    [Function("Get value")]
+    public struct GetValueFunction : IInvokable, IDispatcher
+    {
+        public void Invoke()
+        {
+            // Receive parameters
+            var info = Info();
+            var columnName = ColumnName();
+
+            // Set up parameters
+            Value = info.GetValue(columnName)?.ToString();
+
+            Dispatch();
+        }
+
+        public Action Dispatch { get; set; }
+
+        [Input]
+        public Func<BaseInfo> Info { get; set; }
+
+        [Input(typeof(StringValueBuilder), new object[] { "columnName" })]
+        public Func<string> ColumnName { get; set; }
+
+        [Output]
+        public string Value { get; set; }
+    }
+}
---
+++ TaskBuilder.Functions.Implementations/LogEventFunction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using CMS.EventLog;
+
+using TaskBuilder.Attributes;
+using TaskBuilder.ValueBuilders;
+
+namespace TaskBuilder.Functions.Implementations
+{
+    [Function("Log event", 0, 0, 255)]
+    public struct LogEventFunction : IInvokable
+    {
+        public void Invoke()
+        {
+            // Receive parameters
+            var fromSource = TargetInParameter();
+
+            // Set up parameters
+
+            // Send / Execute
+            EventLogProvider.LogEvent(EventRecordType(), nameof(LogEventFunction), "TESTLOG", fromSource);
+        }
+
+        // This must be linked as a reactive parameter
+        [Input("Description", null, typeof(StringValueBuilder), new object[] { "description" }, new object[] { "default" })]
+        public Func<string> TargetInParameter { get; set; }
+
+        [Input(typeof(StringValueOptionsBuilder), new object[] { EventType.INFORMATION, EventType.WARNING, EventType.ERROR }, new object[] { EventType.WARNING }, true)]
+        public Func<string> EventRecordType { get; set; }
+    }
+}
